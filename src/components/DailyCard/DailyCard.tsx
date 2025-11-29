5129335e26a7a2f213fa832bb10cdb27e2caa593
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-11-29 11:48:53 +0100

    Add simple style to prognosis cards

--- src/components/DailyCard/DailyCard.tsx
+++ src/components/DailyCard/DailyCard.tsx
@@ -1,4 +1,6 @@
 import type { DailyEnergyAverage } from "../../types/DailyEnergyAverage";
+import { capitalize } from "../../util/util";
+import "./DailyCard.css";
 
 type Props = {
   data: DailyEnergyAverage;
@@ -6,16 +8,17 @@ type Props = {
 
 export function DailyCard({ data }: Props) {
   return (
-    <div>
-      <h3>{data.date.toDateString()}</h3>
-      <ul>
+    <div className="daily-card">
+      <h3 className="daily-card__date">{data.date.toDateString()}</h3>
+      <ul className="daily-card__list">
         {data.averages.map((mix, index) => (
-          <li key={index}>
-            {mix.fuel}: {mix.perc.toFixed(0)}%
-          </li>
-        ))}
+        <li key={index} className="daily-card__item">
+          <span className="daily-card__fuel">{capitalize(mix.fuel)}</span>
+          <span className="daily-card__perc">{mix.perc.toFixed(0)}%</span>
+        </li>
+      ))}
       </ul>
-      <h4>Total clean energy usage: {data.totalCleanPerc.toFixed(0)}%</h4>
+      <h4 className="daily-card__clean">Total clean energy: {data.totalCleanPerc.toFixed(0)}%</h4>
     </div>
   );
 }
